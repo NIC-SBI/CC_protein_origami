b7e2028cb628da4298615606d9ab2aa35e68b09d
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2015-10-19 17:30:14 +0200

    Moved modeller script to own repo.

---
+++ Readme.md
@@ -0,0 +1,20 @@
+Scripts for modeling polypeptide polyhedra.
+
+Scripts require Python 2.7 or better with numpy and pandas.
+
+Other dependencies:
+
+* Modeller
+* Chimera
+
+
+The Anaconda python distribution is recommended. 
+
+On the cluster the conda python can be installed using.
+
+```
+###Install conda
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
+bash Anaconda-2.3.0-Linux-x86_64.sh
+#(yes add to path)
+```
---
+++ ppmod/utils.py
@@ -0,0 +1,50 @@
+#taken from http://hayd.github.io/2013/dotable-dictionaries/
+class Dotable(dict):
+    """Generates nested dotable dicts from a json-like object. This makes is possible to write
+    a.b[0].c
+    """
+    __getattr__= dict.__getitem__
+
+    def __init__(self, d):
+        self.update(**dict((k, self.parse(v))
+                           for k, v in d.iteritems()))
+
+    @classmethod
+    def parse(cls, v):
+        if isinstance(v, dict):
+            return cls(v)
+        elif isinstance(v, list):
+            return [cls.parse(i) for i in v]
+        else:
+            return v
+        
+        
+#taken from http://stackoverflow.com/a/13105359/952600
+def byteify(input):
+    """Takes a jason-like structure and convets unicode str to str"""
+    if isinstance(input, dict):
+        return {byteify(key):byteify(value) for key,value in input.iteritems()}
+    elif isinstance(input, list):
+        return [byteify(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input        
+    
+def load_json_data(file_name):
+    """Loads data from json and returns a dotable dict with ascii strings"""
+    import json
+    with open(file_name, 'r') as infile:
+        data=json.load(infile)
+    data=byteify(data)
+    return Dotable.parse(data)
+
+def pair_ids_from_segments(segments):
+    """Return the pair IDs of the segments. The IDs are ZERO based. The matching is based on naming"""
+    pair_ids = []
+    for n in range(len(segments)):
+        for n1 in range(n+1,len(segments)):
+            if segments[n]['name']==segments[n1]['pair_name']:
+                pair_ids.append((n, n1))
+    return pair_ids    
+    
