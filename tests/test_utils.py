22ea25cdc54daf9e299d96b1863574fc94cd5d0d
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2015-11-05 09:36:44 +0100

    added test for spaces in knowns

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,10 +9,15 @@ def test_relative_to():
   assert os.path.isfile(aln_file) 
 
 def test_sequnce_and_knowns():
-  """Test file name handling"""
-  import os
   aln_file = u.relative_to(__file__, 'data/test-aln.ali')
   seq, knowns = u.sequnce_and_knowns(aln_file)
   assert seq=='tet-p5LD3' 
   #use set so order is not important
   assert set(knowns)==set(['p3_p4', 'p5_p6', 'p7_p8', 'APHshort', 'BCRshort', 'GCN'])
+
+def test_sequnce_and_knowns_spaces():
+  aln_file = u.relative_to(__file__, 'data/test-aln-spaces.ali')
+  seq, knowns = u.sequnce_and_knowns(aln_file)
+  assert seq=='tet-p5LD3' 
+  #use set so order is not important
+  assert set(knowns)==set(['p3_p4', 'p5_p6', 'p7_p8', 'APHshort', 'BCRshort', 'GCN'])  
