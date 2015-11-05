a2d3f7fcdb671c0e1053bccb432a44305407b4b2
Janaa <jana.aupic@ki.si>
2015-11-05 11:47:09 +0100

    Added functions needed for generation of alignment file

--- ppmod/make_alignment.py
+++ ppmod/make_alignment.py
@@ -8,7 +8,7 @@ and the new structure is written to a pdb file which is used in homology modelli
 It is advised to check the file before use in subsequent modelling.
 """
 
-from __future__ import print_function
+from __future__ import print_function, absolute_import
 import mdtraj as md
 import argparse
 import re
@@ -17,182 +17,64 @@ import os
 import numpy as np
 from modeller import *
 
-parser = argparse.ArgumentParser(__doc__,
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('-j', '--json', help="json file name", default='data.json')
-parser.add_argument('-aln', '--alignment', help="alignment file name", default='alignment-file.ali')
-parser.add_argument('-p', '--path', help="path to template pdb files", default='../../building_blocks/')
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(__doc__,
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-j', '--json', help="json file name", default='data.json')
+    parser.add_argument('-aln', '--alignment', help="alignment file name", default='alignment-file.ali')
+    parser.add_argument('-p', '--path', help="path to template pdb files", default='../../building_blocks/')
+    args = parser.parse_args()
 
-def align(p1f, seq, j, jj):
-    """Align the template sequence to the target sequence
+    d = u.load_json_data(args.json)         #read json file
+    aln_str = d.entire_sequence
 
-    Parameters
-    ----------
-    p1f : str
-        Template sequence string
-    seq : str
-        Target sequence string
-    j : int
-        How many different template sequences are checked for optimum alignment (different sequences are obtained by sequentially shortening p1f by one AA)  
-    jj : int  
-        How many different target sequences are checked for optimum alignment (different sequences are obtained by sequentially shortening seq by one AA)
+    with open(args.alignment, 'w') as f1:       
+        for pair, pair2 in d.pairs:  #go through all CC pairs
+            p1, p2, pdbname = u.find_pair(pair, d.segments)
+            topology = md.load(os.path.join(args.path, pdbname)).topology   #read topology
+            position = md.load(os.path.join(args.path, pdbname)).xyz        #and position from pdb file  
+            p1f = topology.to_fasta(0)
+            p2f = topology.to_fasta(1)             #convert topology to fasta sequence
+            
+            #align template structures to target check weather the template sequence is to long and determine alignemnt positions by checking the quality of different alignments
+            i, ii = u.align(p1f, d.segments[p1]['sequence'], 6, 6)            
+            
+ 
 
-    Returns
-    -------
-    i : int
-        Start of the alignment on template sequence 
-    ii : int
-        Start of the alignment on target sequence
-    """    
-    i = 0
-    ii = 0
-    scoreold = score(p1f, seq)
-    for t in range(j):   
-       for tt in range(jj): 
-           scorenew = score(p1f[t:], seq[tt:])  
-           if scorenew > scoreold:
-               scoreold = scorenew
-               i = t
-               ii = tt
-    return (i, ii)
-
-def score(p1f, seq):
-    """This function scores the quality of the alignment. Higher score means better alignment
-
-    Parameters
-    ----------
-    p1f : str
-        Template sequence string
-    seq : str
-        Target sequence string
-    
-    Returns
-    -------
-    total : int
-        Final score of the alignmnet
-    """
-    total = 0
-    for i in range(8):  #check the first ten residues
-        if p1f[i] == seq[i]:   #if p1f and seq have a matching residue on position i add a point
-            total = total+1  
-    return total                          #return final score
-
-def find_pair(pair):
-     """This function scores the quality of the alignment. Higher score means better alignment
-
-    Parameters
-    ----------
-    pair : str
-        Name of CC pair
-    
-    Returns
-    -------
-    p1 : int
-        Id number of pair in 
-    p2 : int
-    pdbname : str
-    """
-    for s in d.segments:
-        if s['name'] == pair:
-            p1 = s[n]['id']-1         #get segment name
-            p2 = s[n]['pair_id']-1
-            pdbname = s['pdb_template']   #get template pdb file name
-            break     
-    return p1, p2, pdbname
-
-def selres(i, t):
-    """Select i-th residue from topology
-
-    Parameters
-    ----------
-    i : int
-        Residue index
-    t : mdtraj topology
-        Topology
-    
-    Returns
-    -------
-    res : mdtraj residue
-        i-th residue
-    """
-    res = t.select("resid {:d}".format(i))
-    return res
-
-def writepdb(i, l, t, p, path):
-    """Write the aligned part of the template sequence to pdb
-
-    Parameters
-    ----------
-    i : int
-        Start of the aligning part on the template sequence
-    l : int
-        Length of the aligned template sequence
-    t : mdtraj topology
-        topology of the template 
-    p : md traj positions
-        atom positions in template    
-    """
-    cl0 = t.chain(0).n_residues  #cl0 length of the first chain of a CC pair
-    topsub1 = t.subset(list(range(selres(i, t)[0],selres(i+l, t)[-1])))
-    topsub2 = t.subset(list(range(selres(cl0+i, t)[0], selres(cl0+i+l, t)[-1])))   
-    topsubjoin = topsub1.join(topsub2)
-    coord = np.concatenate((p[0, selres(i, t)[0]:selres(i+l, t)[-1], :]*10, p[0, selres(cl0+i, t)[0]:selres(cl0+i, t)[-1], :]*10), axis=0)
-    fpdb = md.formats.PDBTrajectoryFile(path, mode='w')
-    fpdb.write(coord, topsubjoin)
-    return
-
-d = u.load_json_data(args.json)         #read json file
-aln_str = d.entire_sequence
-
-with open(args.alignment, 'w') as f1:
-    for pair in d.pairs:  #go through all CC pairs
-        p1, p2, pdbname=find_pair(pair[0])
-        topology = md.load(os.path.join(args.path, pdbname)).topology   #read topology
-        position = md.load(os.path.join(args.path, pdbname)).xyz        #and position from pdb file  
-        p1f = topology.to_fasta(0)
-        p2f = topology.to_fasta(1)             #convert topology to fasta sequence
-        
-        #align template structures to target
-        #check weather the template sequence is to long and determine alignemnt positions by checking the quality
-        #of different alignments
-             
-        i, ii = align(p1f, d.segments[p1]['sequence'], 6, 6)            
-        l = len(min((p1f[i:], d.segments[p1]['sequence'][ii:]), key=len)) 
-
-        #shorten the template sequence if needed and write the topology and the coordinates to a new pdb file
-        path = (os.path.join(args.path, pair[0]+'-new.pdb'))        
-        writepdb(i, l-1, topology, positions, path)
-       
-        #write the alignment file taking account previously determined alignemnt position
-        count = 0
-        f1.write('>P1;{}\n'.format(pdbname.rpartition('.')[0]))    
-        f1.write('structureX:{}::A:::::-1.00:-1.00\n'.format(pair[0]+'-new.pdb'))
-        
-        print('>P1;{}'.format(pdbname.rpartition('.')[0]))    
-        print('structureX:{}::A:::::-1.00:-1.00'.format(pair[0]+'-new.pdb'))
-        while count < len(aln_str):
-            if count == s[p1]['start']-1+ii:
-                f1.write(p1f[i:i+l])
-                print(p1f[i:i+l])
-                count = count+len(p1f[i:i+l])
-            elif count == s[p2]['start']-1+ii:
-                f1.write(p2f[i:i+l])
-                print(p2f[i:i+l])
-                count = count+len(p2f[i:i+l])
-            else:
-                f1.write("-")
-                print("-", end="")
-                count = count +1
-        print('*')
-        print("")
+            #shorten the template sequence if needed and write the topology and the coordinates to a new pdb file
+            l = len(min((p1f[i:], d.segments[p1]['sequence'][ii:]), key=len)) #compare the length of aligned template and target sequence and get the length of teh shorter one
+            path = (os.path.join(args.path, pair+'-new.pdb')) #path to new pdb files      
+            u.writepdb(i, l-1, topology, position, path)
+           
+            #write the alignment file taking into account previously determined alignment position
+            count = 0
+            f1.write('>P1;{}\n'.format(pair))    
+            f1.write('structureX:{}::A:::::-1.00:-1.00\n'.format(pair + '-new.pdb'))
+            
+            print('>P1;{}'.format(pair))    
+            print('structureX:{}::A:::::-1.00:-1.00'.format(pair + '-new.pdb'))
+            while count < len(aln_str):
+                if count == d.segments[p1]['start']-1 + ii:
+                    f1.write(p1f[i : i + l])
+                    print(p1f[i : i + l])
+                    count = count+len(p1f[i : i + l])
+                elif count == d.segments[p2]['start']-1 + ii:
+                    f1.write(p2f[i : i + l])
+                    print(p2f[i : i + l])
+                    count = count+len(p2f[i : i + l])
+                else:
+                    f1.write("-")
+                    print("-", end="")
+                    count = count +1
+            print('*')
+            print("")
+            f1.write('*\n')
+            f1.write('\n')
+        f1.write('>P1;{}\n'.format(d.name))
+        f1.write('sequence:{}:1:A::::: 0.00:0.00\n'.format(d.name))
+        f1.write(aln_str)
         f1.write('*\n')
-        f1.write('\n')
-    f1.write('>P1;{}\n'.format(d.name))
-    f1.write('sequence:{}:1:A::::: 0.00:0.00\n'.format(d.name))
-    f1.write(aln_str)
-    f1.write('*\n')
-    print('>P1;{}\n'.format(d.name), end="")
-    print('sequence:{}:1:A::::: 0.00:0.00\n'.format(d.name), end="")
-    print(aln_str, end="")
-    print('*\n', end="")
+        print('>P1;{}\n'.format(d.name), end="")
+        print('sequence:{}:1:A::::: 0.00:0.00\n'.format(d.name), end="")
+        print(aln_str, end="")
+        print('*\n', end="")
--- ppmod/utils.py
+++ ppmod/utils.py
@@ -9,7 +9,7 @@ class Dotable(dict):
 
     def __init__(self, d):
         self.update(**dict((k, self.parse(v))
-                           for k, v in d.iteritems()))
+                           for k, v in d.items()))
 
     @classmethod
     def parse(cls, v):
@@ -25,7 +25,7 @@ class Dotable(dict):
 def byteify(input):
     """Takes a jason-like structure and convets unicode str to str"""
     if isinstance(input, dict):
-        return {byteify(key):byteify(value) for key,value in input.iteritems()}
+        return {byteify(key):byteify(value) for key,value in input.items()}
     elif isinstance(input, list):
         return [byteify(element) for element in input]
     elif isinstance(input, unicode):
@@ -38,7 +38,7 @@ def load_json_data(file_name):
     import json
     with open(file_name, 'r') as infile:
         data=json.load(infile)
-    data=byteify(data)
+#    data=byteify(data)
     return Dotable.parse(data)
 
 def pair_ids_from_segments(segments):
@@ -53,6 +53,8 @@ def pair_ids_from_segments(segments):
 
 import string
 import random
+import numpy as np
+import mdtraj as md
 #taken from http://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))    
@@ -84,3 +86,125 @@ def relative_to(file_dir, a_path):
     #os.path.join(os.getcwd(), os.path.dirname(__file__)))
     a_dir = os.path.dirname(file_dir)
     return os.path.join(a_dir,a_path)
+
+def align(p1f, seq, j, jj):
+    """Align the template sequence to the target sequence
+
+    Parameters
+    ----------
+    p1f : str
+        Template sequence string
+    seq : str
+        Target sequence string
+    j : int
+        How many different template sequences are checked for optimum alignment (different sequences are obtained by sequentially shortening p1f by one AA)  
+    jj : int  
+        How many different target sequences are checked for optimum alignment (different sequences are obtained by sequentially shortening seq by one AA)
+
+    Returns
+    -------
+    i : int
+        Start of the alignment on template sequence 
+    ii : int
+        Start of the alignment on target sequence
+    """    
+    i = 0
+    ii = 0
+    scoreold = score(p1f, seq)
+    for t in range(j):   
+       for tt in range(jj): 
+           scorenew = score(p1f[t:], seq[tt:])  
+           if scorenew > scoreold:
+               scoreold = scorenew
+               i = t
+               ii = tt
+    return (i, ii)
+
+def score(p1f, seq):
+    """This function scores the quality of the alignment. Higher score means better alignment
+
+    Parameters
+    ----------
+    p1f : str
+        Template sequence string
+    seq : str
+        Target sequence string
+    
+    Returns
+    -------
+    total : int
+        Final score of the alignmnet
+    """
+    total = 0
+    for i in range(8):  #check the first ten residues
+        if p1f[i] == seq[i]:   #if p1f and seq have a matching residue on position i add a point
+            total = total+1  
+    return total                          #return final score
+
+def find_pair(pair, segments):
+    """Find pair's id, pair_id, and its pdb file
+
+    Parameters
+    ----------
+    pair : str
+        Name of CC segments
+    
+    Returns
+    -------
+    p1 : int
+        Id number of the CC segment 
+    p2 : int
+        Pair_id number of the CC segment
+    pdbname : str
+        Name of the pdb file where the CC segment is written
+    """
+    for s in segments:
+        if s['name'] == pair:
+            p1 = s['id']-1         #get segment name
+            p2 = s['pair_id']-1
+            pdbname = s['pdb_template']   #get template pdb file name
+            break     
+    return p1, p2, pdbname
+
+def selres(i, t):
+    """Select i-th residue from topology
+
+    Parameters
+    ----------
+    i : int
+        Residue index
+    t : mdtraj topology
+        Topology
+    
+    Returns
+    -------
+    res : mdtraj residue
+        i-th residue
+    """
+    res = t.select("resid {:d}".format(i))
+    return res
+
+def writepdb(i, l, t, p, path):
+    """Write the aligned part of the template sequence to pdb
+
+    Parameters
+    ----------
+    i : int
+        Start of the aligning part on the template sequence
+    l : int
+        Length of the aligned template sequence
+    t : mdtraj topology
+        topology of the template 
+    p : md traj positions
+        atom positions in template    
+    """
+    cl0 = t.chain(0).n_residues  #cl0 length of the first chain of a CC pair
+    topsub1 = t.subset(list(range(selres(i, t)[0],selres(i + l, t)[-1]))) #topology of atoms in the first aligned chain of the CC segment
+    topsub2 = t.subset(list(range(selres(cl0 + i, t)[0], selres(cl0 +i + l, t)[-1]))) #topology of atoms in the first aligned chain of the CC segment
+    topsubjoin = topsub1.join(topsub2) # join topology subsets
+    coord1 = p[0, selres(i, t)[0]:selres(i + l, t)[-1], :]*10 #position of atoms in the first subset
+    coord2 = p[0, selres(cl0 + i, t)[0]:selres(cl0 + i + l, t)[-1], :]*10 #position of atoms in the second subset
+    coord = np.concatenate((coord1, coord2), axis=0) #join position subsets
+    fpdb = md.formats.PDBTrajectoryFile(path, mode='w') 
+    fpdb.write(coord, topsubjoin) #write to pdb
+    return
