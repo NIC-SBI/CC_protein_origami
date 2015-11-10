62b5da0be5662003890ab8daa73db0a6edf5c989
Janaa <jana.aupic@ki.si>
2015-11-10 15:03:45 +0100

    changed test, included biopython

--- tests/test_make_alignment.py
+++ tests/test_make_alignment.py
@@ -3,6 +3,12 @@ import ppmod
 def test_make_alignemnt():
     import subprocess
     import filecmp
+    import Bio.SeqIO
     cmd_line = "python ../ppmod/make_alignment.py --json data/APHsh.json -aln data/APHtest.ali > out.log"
     subprocess.call(cmd_line, shell=True)
-    assert filecmp.cmp('data/APHsh.ali', 'data/APHtest.ali')
+    with open('data/APHtest.ali', 'r') as f1:
+        li = []
+        for i in Bio.SeqIO.PirIO.PirIterator(f1):
+            li.append(i.seq)
+    assert li[0] == "ELKQLEEELQAIEEQLAQLQWKAQARKEKLAQLK/-----ELKQLEEELQAIEEQLAQLQWKAQARKEKLAQLK"
+    assert li[1] == "ELKQLEEELQAIEEQLAQLQWKAQARKEKLAQLKGSGSGSELKQLEEELQAIEEQLAQLQWKAQARKEKLAQLK"
