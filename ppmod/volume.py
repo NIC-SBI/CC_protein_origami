2da9f1632312e3b819e9842e11f0cd851ae02cc4
janaaupic <jana.aupic@ki.si>
2015-12-16 17:18:18 +0100

    Script for calculating volume of the CC polyhedra, code for writing vertices to pdb file will be addded shortly

---
+++ ppmod/volume.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+"""Script for calculating approximate volume of coiled-coil polyhedra
+
+Requires: SciPy v0.17.0
+"""
+from scipy.spatial import ConvexHull
+import numpy as np
+import mdtraj as md
+import utils as u
+import argparse
+
+parser = argparse.ArgumentParser(description="Script for calculating approximate volume of the polyhedra")
+parser.add_argument('-m', '--model', help='Input file name (pdb)', type=str)
+parser.add_argument('-j', '--json', help='JSON file defining segments configuration', type=str)
+args = parser.parse_args()
+
+#read input files
+d = u.load_json_data(args.json)
+md_obj = md.load(args.model)
+
+#read topology and position
+topology = md_obj.topology
+positions = md_obj.xyz 
+#converting positions from 3D np array into a nested list
+positions = np.reshape(positions, (topology.n_atoms, 3))
+positions = positions.tolist()
+
+points = [] #points that will be used for ConvexHull calculation
+for pair in d.pairs:  #go through all CC pairs
+    pair_1_id, pair_2_id, pdbname = u.find_pair(pair['pair'][0], d.segments)
+    p1_start = d.segments[pair_1_id]['start'] 
+    p1_end = d.segments[pair_1_id]['end']
+    p2_start = d.segments[pair_2_id]['start'] 
+    p2_end = d.segments[pair_2_id]['end']
+    #select CA atoms in the starting and ending residues of both pair segments 
+    atom_1 = positions[topology.select("residue " + str(p1_start) + " and name CA")]
+    atom_2 = positions[topology.select("residue " + str(p1_end) + " and name CA")]
+    atom_3 = positions[topology.select("residue " + str(p2_start) + " and name CA")]
+    atom_4 = positions[topology.select("residue " + str(p2_end) + " and name CA")]
+    #from atom positions specify the aproximate coordinates of the start and end of the CC pair
+    if d.pair_types[d.pairs.index(pair)] == 'A':
+            avg1 = [(atom_1[i] + atom_4[i])/2 for i in range(len(atom_1))]
+            avg2 = [(atom_2[i] + atom_3[i])/2 for i in range(len(atom_1))]
+    else:
+        avg1 = [(atom_1[i] + atom_3[i])/2 for i in range(len(atom_1))]
+        avg2 = [(atom_2[i] + atom_4[i])/2 for i in range(len(atom_1))]
+    #add the coordinates of the start and end of the CC pair to list of points
+    points.append(avg1)
+    points.append(avg2)
+points = np.array(points)
+hull = ConvexHull(points, qhull_options='Qt')
+print(hull.volume)
+#for simplex in hull.simplices:
+#    print(points[simplex, 0], points[simplex, 1], points[simplex, 2])     
+for simplex in hull.vertices:
+    print(points[simplex, 0], points[simplex, 1], points[simplex, 2])   
