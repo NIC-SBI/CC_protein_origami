4cffb9abf585c0c22577523396e5dc0861a45e73
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2016-03-23 16:49:15 +0100

    Coloring script for chimera is complete.

---
+++ ppmod/make_color_script.py
@@ -0,0 +1,36 @@
+
+# -*- coding: utf-8 -*-
+"""
+Given a JSON file generate scripts for coloring the representation in Chimera and VMD.
+"""
+from __future__ import print_function
+import utils as u
+import yaml
+import six
+import re
+
+def chimera_color(json_file, out_file, model_number="", verbose=True):
+    d=u.load_json_data("pyr-p1b-SN-N5.json")
+
+    with open(out_file+'chimera', 'a') as f.
+        for s in d.segments:
+            template = """color {color} #{model}:{start}-{end}"""
+            s['model']=''
+            line = template.format(**s)
+            if verbose:
+                print(line)
+            f.write(line)
+
+
+
+if __name__ == "main":
+    import argparse    
+    parser=argparse.ArgumentParser(__doc__)
+    parser.add_argument("-j","--json", default='data.json', help='specify path to json file')
+    parser.add_argument("-o","--out-file", default='color', help='Output name (without extension) of the coloring script')
+    parser.add_argument("-v","--verbose", default='color', help='print scripts to screen')
+    
+
+    a = parser.parse_args()
+
+    chimera_color(a.json, a.output, verbose=a.verbose)
