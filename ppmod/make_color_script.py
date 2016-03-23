d81e04f0f1f79d2fed5acc17798bfa2e3802b234
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2016-03-23 17:01:25 +0100

    Some fixes to make_color_script.py

--- ppmod/make_color_script.py
+++ ppmod/make_color_script.py
@@ -12,7 +12,7 @@ import re
 def chimera_color(json_file, out_file, model_number="", verbose=True):
     d=u.load_json_data("pyr-p1b-SN-N5.json")
 
-    with open(out_file+'chimera', 'a') as f.
+    with open(out_file+'.chimera', 'w') as f:        
         for s in d.segments:
             template = """color {color} #{model}:{start}-{end}"""
             s['model']=''
@@ -22,15 +22,13 @@ def chimera_color(json_file, out_file, model_number="", verbose=True):
             f.write(line)
 
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     import argparse    
     parser=argparse.ArgumentParser(__doc__)
     parser.add_argument("-j","--json", default='data.json', help='specify path to json file')
     parser.add_argument("-o","--out-file", default='color', help='Output name (without extension) of the coloring script')
-    parser.add_argument("-v","--verbose", default='color', help='print scripts to screen')
+    parser.add_argument("-v","--verbose", default=True, help='print scripts to screen')
     
 
     a = parser.parse_args()
-
-    chimera_color(a.json, a.output, verbose=a.verbose)
+    chimera_color(a.json, a.out_file, verbose=a.verbose)
