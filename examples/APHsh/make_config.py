6038bee1e9eb680eca8f47b69cf41cccacfaa2da
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2016-08-10 12:54:55 +0200

    APHsh example update, made consistent with the readme...

--- examples/APHsh/make_config.py
+++ examples/APHsh/make_config.py
@@ -1,21 +1,19 @@
-name = "APHsh"
-entire_sequence ="ELKQLEEELQAIEEQLAQLQWKAQARKEKLAQLKGKGDGELKQLEEELQAIEEQLAQLQWKAQARKEKLAQLK"
-
-segments = """
+model_name = "APHsh"
+annotated_sequence = """
 ELKQLEE ELQAIEE QLAQLQW KAQARKE KLAQLK 		APHshort
 GKGDG
 ELKQLEE ELQAIEE QLAQLQW KAQARKE KLAQLK 		APHshort
 """
-
-pairs = """
+pairs_info = """
 - pair : APHshort:APHshort
   type : A  
   chains : A:B
   template : APH.pdb  
 """   
 
-
 if __name__ == "__main__":
     import ppmod.make_json as mj   
-    pairs = mj.load_pairs(pairs)    
-    mj.generate_json(name, entire_sequence, segments, pairs)
+    import ppmod.segment_assignment as sa
+    entire_sequence = sa.deannotate_sequence(annotated_sequence, remove_whitespace=True)  
+    pairs = mj.load_pairs(pairs_info)    
+    mj.generate_json(model_name, entire_sequence, annotated_sequence, pairs)
