c8fc6f7ea32fc54e7e111f6d65abcc9b933bae75
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2016-09-05 14:34:49 +0200

    Updated APHsh make_config.py to print color script and fasta file.

--- examples/APHsh/make_config.py
+++ examples/APHsh/make_config.py
@@ -14,6 +14,19 @@ pairs_info = """
 if __name__ == "__main__":
     import cocopod.make_json as mj   
     import cocopod.segment_assignment as sa
+    import cocopod.make_color as mc
+    import cocopod.utils as u
     entire_sequence = sa.deannotate_sequence(annotated_sequence, remove_whitespace=True)  
     pairs = mj.load_pairs(pairs_info)    
+
+    #generate json file 
     mj.generate_json(model_name, entire_sequence, annotated_sequence, pairs)
+    print("Written: "+model_name+".json")    
+    
+    #create chimera script for coloring.
+    mc.chimera_color(model_name+".json", model_name+".chimera", verbose=False)
+    print("Written: "+model_name+".chimera")
+    
+    #write fasta file
+    u.write_fasta_file(model_name+".fasta", model_name, entire_sequence)
+    print("Written: "+model_name+".fasta")
